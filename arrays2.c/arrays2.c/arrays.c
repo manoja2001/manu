248fe897a10e23794dfea8feb29ab25c952582c3
Manoja Manu <manojamanoja0204@gmail.com>
2024-12-04 18:16:54 +0530

    arrays program

---
+++ arrays2.c/arrays2.c/arrays.c
@@ -0,0 +1,19 @@
+//#include<stdio.h>
+//void main() 
+//{
+//    int i;
+//    int a[5];
+//	a[0]=10;
+//    a[1]=20;
+//	a[2]=30;
+//	printf("%d:%d/n",a[i],&a[i]);
+//    for (i = 0; i < 5; i++) 
+//	{
+//        scanf("%d", &a[i]);
+//   }
+//    for (i = 0; i < 5; i++) 
+//
+//	{
+//        printf("%d", a[i]);
+//    }
+//}
